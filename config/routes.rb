2ea1047044b08ac4ba97c01f1acce683f39821c3
Steven Gutierrez <stevogbmv@gmail.com>
2023-08-22 12:35:44 -0700

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   get '/hello', to: 'application#hello_world'
+  get '*path', 
+    to: 'fallback#index', 
+    constraints: ->(req) { !rez.xhr? && req.format.html? }
 end
